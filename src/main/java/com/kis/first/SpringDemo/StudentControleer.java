6b1209c96946a845ed84e73945ac47480b24cf14
Chandrakant <ichandrakant08@gmail.com>
2022-12-07 15:24:36 +0530

    ticket 03
    response entity

---
+++ src/main/java/com/kis/first/SpringDemo/StudentControleer.java
@@ -0,0 +1,29 @@
+package com.kis.first.SpringDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentControleer {
+	
+	@RequestMapping("/first")
+	public String welcomeMassage(){
+		return "ok";
+		
+	}
+	@GetMapping("/second")
+	public ResponseEntity<String> getStringname(@pathVariable String second){
+	String name= "Kiran";
+	if (String = Kiran){
+	return new ResponseEntity<String>(name, HttpStatus.OK);
+	}
+	else{
+	 return new ResponseEntity<String>(null, HttpStatus.NO_CONTENT);
+	}
+
+
+
+
+}
+
+}
